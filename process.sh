409d2ecbe775c0d51dfac08cafd1864347d6790a
gabyfulchic <gaby.fulchic@ynov.com>
2019-05-29 18:14:30 +0200

    test some things

--- process.sh
+++ process.sh
@@ -1,7 +1,5 @@
 #/bin/env bash
 
-# Some tests
-
 logfile="/tmp/process-error.log"
 touch $logfile
 echo "[`date +"%d-%m-%Y %T"`]" >> $logfile
@@ -9,6 +7,7 @@ echo "[`date +"%d-%m-%Y %T"`]" >> $logfile
 if [ $UID -ne 42 ];
 then
 	echo "Hum it seems that your user is the wrong." >> $logfile
+	exit 1
 else
 	echo "User is OK." >> $logfile
 fi
@@ -17,10 +16,19 @@ ansible --help
 if [ $? -ne 0 ];
 then
 	echo "Ansible is not well installed in the container." >> $logfile
+	exit 1
 else
 	echo "Ansible is successfully working." >> $logfile
 fi
 
 # Dans le cas de l'ajustement des templates de mail 3CX
 # ansible-playbook -i remote-hosts.ini playbooks/playbook-3cxtemplates.yml 
+
+# Si on dev les playbooks avec le module check_mode: yes (not make any changes on remote systems)
+# A utiliser par exemple pour tester les modules ansible, et appliquer des changements suivant le résulat : register.
+# ansible-playbook --check -i remote-hosts.ini playbooks/playbook-3cxtemplates.yml
+
+# Si on veut tester la syntaxe plus généralement
+ansible-playbook --syntax-check --verbose playbooks/playbook-3cxtemplates.yml >> $logfile
+
 
