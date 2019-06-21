4e64835d0d09fc285c4f169da5c7287244513418
gabyfulchic <gaby.fulchic@ynov.com>
2019-06-21 15:16:06 +0000

    Dockerfile updated, tree structure changed

--- toBuild/process.sh
+++ toBuild/process.sh
@@ -1,17 +1,20 @@
-#/bin/env bash
+#!/bin/bash
 
 ## Dans le cas du lancement du playbook principal
-# ansible-playbook -i remote-hosts.ini playbooks/random-playbook.yml 
+# ansible-playbook -i inventory.ini playbooks/random-playbook.yml 
 
 ## Si on dev les playbooks avec le module check_mode: yes (not make any changes on remote systems).
 ## A utiliser par exemple pour tester les modules ansible, et appliquer des changements suivant le résulat : register
-# ansible-playbook --check -i remote-hosts.ini playbooks/random-playbook.yml
+# ansible-playbook --check -i inventory.ini playbooks/random-playbook.yml
 
 ## Si on veut tester la syntaxe d'un des playbooks
+resultsFolder="/home/gabyfulchic/.results/"
 resultsFile="/home/gabyfulchic/.results/file"
-mkdir -p $resultsFile
-echo "stdout to resultsFile by >>" >> $resultsFile
-ansible-playbook --syntax-check --verbose playbooks/random-playbook.yml >> $resultsFile
-echo "stdout to resultsFile by | tee" >> $resultsFile
-ansible-playbook --syntax-check --verbose playbooks/random-playbook.yml | tee $resultsFile
- 
+
+sudo mkdir -p $resultsFolder &&\
+	sudo touch $resultsFile &&\
+	sudo chown gabyfulchic:admins -R $resultsFolder
+
+echo 'stdout to resultsFile by >>' >> $resultsFile
+ansible all --list-hosts
+ansible-playbook -i remote-hosts.ini --syntax-check --verbose ansible/playbooks/random-playbook.yml >> $resultsFile
